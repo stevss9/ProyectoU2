dc9dc2842e12110fcffaf3e00a673ea12a3448c4
Steveen <sinchiguanomoreira@gmail.com>
2022-07-15 23:44:26 -0500

    Pagina principal y esquema del juego

---
+++ gamecards.py
@@ -0,0 +1,27 @@
+#Importamos las librerias
+import os
+from flask import Flask, redirect, render_template, request, url_for, flash
+
+
+#Objeto para inicilizar la aplicacion
+app = Flask(__name__)
+#Clave  de la app
+app.secret_key = "123456"
+app.debug = False
+app._static_folder = os.path.abspath("templates/static/")
+
+#Clave  de la app
+app.secret_key = '090700'
+
+#Controlador de la ruta inicial
+@app.route('/')
+def Index():
+    return render_template('Index.html')
+
+@app.route('/juego')
+def juego():
+    return render_template('juego.html')
+
+#Main de la app
+if __name__ == '__main__':
+    app.run(debug=True)
